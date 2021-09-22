a3f775fdfb9df3be58dfefb524bb4db7b189ed0c
hirunagrad <hirunagrad@gmail.com>
2021-09-23 03:25:05 +0530

    create loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:bmic/screen/home.dart';
 import 'package:flutter/material.dart';
+import 'package:bmic/screen/home.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+final Color accentHexColor = HexColor("#FCC91C");
+final Color mainHexColor = HexColor("#1C1C1C");
 
 void main() {
   runApp(MyApp());
@@ -18,7 +22,62 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.yellow,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(),
+      home: LoadingPage(),
+    );
+  }
+}
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 5),
+      () {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: mainHexColor,
+        height: double.infinity,
+        width: double.infinity,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "BMI",
+                style: TextStyle(
+                    color: accentHexColor,
+                    fontSize: 85,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'ubuntu'),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 40,
+              ),
+              Container(
+                child: SpinKitThreeBounce(
+                  color: Colors.white,
+                  size: 40.0,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
